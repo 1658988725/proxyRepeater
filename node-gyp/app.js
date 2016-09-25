cb21ccb55605cc22cd405e38bc4486aedb0473f3
zhongkui <zhongkui@139.com>
2016-09-25 15:07:18 +0800

    midify node-gyp/app.js
    Signed-off-by: zhongkui <zhongkui@139.com>

--- node-gyp/app.js
+++ node-gyp/app.js
@@ -1,16 +1,12 @@
 var nvr = require('./build/Release/node_nvr_addon.node');
 
 var data = [{
-        rtspUrl: "rtsp://10.196.230.149/000100",
-        rtspUser: "admin",
-        rtspPass: "12345",
+        url: "rtsp://admin:12345@10.196.230.149/000100",
         endpoint: "rtmp://127.0.0.1:1935/srs",
         stream: "stream1",
         password: "publish2016"
    }, {
-	    rtspUrl: "rtsp://10.196.230.150/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif",
-        rtspUser: "888888",
-        rtspPass: "888888",
+	    rtspUrl: "rtsp://888888:888888@10.196.230.150/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif",
 	    rtspUseTcp: 0,
         endpoint: "rtmp://127.0.0.1:1935/srs",
         stream: "stream2",
