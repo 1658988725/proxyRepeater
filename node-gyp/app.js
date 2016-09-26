d387f30e4176d334e5dd46c1582ebd512d1dca62
zhongkui <zhongkui@139.com>
2016-09-26 08:56:20 +0800

    modify node-gyp/app.js and objs/srs.conf

--- node-gyp/app.js
+++ node-gyp/app.js
@@ -1,12 +1,12 @@
 var nvr = require('./build/Release/node_nvr_addon.node');
 
 var data = [{
-        url: "rtsp://admin:12345@10.196.230.149/000100",
-        endpoint: "rtmp://127.0.0.1:1935/srs",
-        stream: "stream1",
-        password: "publish2016"
-   }, {
-	    rtspUrl: "rtsp://888888:888888@10.196.230.150/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif",
+    url: "rtsp://10.196.230.149/000100",
+    endpoint: "rtmp://127.0.0.1:1935/srs",
+    stream: "stream1",
+    password: "publish2016"
+}, {
+	    url: "rtsp://888888:888888@10.196.230.150/cam/realmonitor?channel=1&subtype=0",
 	    rtspUseTcp: 0,
         endpoint: "rtmp://127.0.0.1:1935/srs",
         stream: "stream2",
