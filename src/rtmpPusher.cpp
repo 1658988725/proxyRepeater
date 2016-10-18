70a6d6fdf386bc5523224c599a8f435dcc7fb589
zhongkui <zhongkui@139.com>
2016-10-18 16:36:52 +0800

    fixed: sps width&height calc Signed-off-by: zhongkui <zhongkui@139.com>

--- src/rtmpPusher.cpp
+++ src/rtmpPusher.cpp
@@ -110,186 +110,6 @@ int parseConfData(cJSON* conf) {
 	return iCount;
 }
 
-int h264_decode_sps(BYTE * buf, unsigned int nLen, unsigned &width, unsigned &height, unsigned &fps) {
-	UINT StartBit = 0;
-	unsigned chroma_format_idc = 0;
-	unsigned frame_crop_left_offset;
-	unsigned frame_crop_right_offset;
-	unsigned frame_crop_top_offset;
-	unsigned frame_crop_bottom_offset;
-	fps = 0;
-	de_emulation_prevention(buf, &nLen);
-
-	//forbidden_zero_bit =
-	u(1, buf, StartBit);
-	//nal_ref_idc =
-	u(2, buf, StartBit);
-	int nal_unit_type = u(5, buf, StartBit);
-	if (nal_unit_type == 7) {
-		int profile_idc = u(8, buf, StartBit);
-		//constraint_set0_flag =
-		u(1, buf, StartBit); //(buf[1] & 0x80)>>7;
-		//constraint_set1_flag =
-		u(1, buf, StartBit); //(buf[1] & 0x40)>>6;
-		//constraint_set2_flag =
-		u(1, buf, StartBit); //(buf[1] & 0x20)>>5;
-		//constraint_set3_flag =
-		u(1, buf, StartBit); //(buf[1] & 0x10)>>4;
-		//reserved_zero_4bits =
-		u(4, buf, StartBit);
-		//level_idc =
-		u(8, buf, StartBit);
-
-		//seq_parameter_set_id =
-		Ue(buf, nLen, StartBit);
-
-		if (profile_idc == 100 || profile_idc == 110 || profile_idc == 122
-				|| profile_idc == 144) {
-			chroma_format_idc = Ue(buf, nLen, StartBit);
-			if (chroma_format_idc == 3)
-				//residual_colour_transform_flag =
-				u(1, buf, StartBit);
-			//bit_depth_luma_minus8 =
-			Ue(buf, nLen, StartBit);
-			//bit_depth_chroma_minus8 =
-			Ue(buf, nLen, StartBit);
-			//qpprime_y_zero_transform_bypass_flag =
-			u(1, buf, StartBit);
-
-			int seq_scaling_matrix_present_flag = u(1, buf, StartBit);
-			if (seq_scaling_matrix_present_flag) {
-				for (int i = 0; i < 8; i++) {
-					//seq_scaling_list_present_flag[i] =
-					u(1, buf, StartBit);
-				}
-			}
-		}
-		//log2_max_frame_num_minus4 =
-		Ue(buf, nLen, StartBit);
-		int pic_order_cnt_type = Ue(buf, nLen, StartBit);
-		if (pic_order_cnt_type == 0)
-			//log2_max_pic_order_cnt_lsb_minus4 =
-			Ue(buf, nLen, StartBit);
-		else if (pic_order_cnt_type == 1) {
-			//delta_pic_order_always_zero_flag =
-			u(1, buf, StartBit);
-			//offset_for_non_ref_pic =
-			Se(buf, nLen, StartBit);
-			//offset_for_top_to_bottom_field =
-			Se(buf, nLen, StartBit);
-			int num_ref_frames_in_pic_order_cnt_cycle = Ue(buf, nLen, StartBit);
-
-			int *offset_for_ref_frame = new int[num_ref_frames_in_pic_order_cnt_cycle];
-			for (int i = 0; i < num_ref_frames_in_pic_order_cnt_cycle; i++)
-				offset_for_ref_frame[i] = Se(buf, nLen, StartBit);
-			delete[] offset_for_ref_frame;
-		}
-
-		//num_ref_frames =
-		Ue(buf, nLen, StartBit);
-		//gaps_in_frame_num_value_allowed_flag =
-		u(1, buf, StartBit);
-		int pic_width_in_mbs_minus1 = Ue(buf, nLen, StartBit);
-		int pic_height_in_map_units_minus1 = Ue(buf, nLen, StartBit);
-
-		int frame_mbs_only_flag = u(1, buf, StartBit);
-		if (!frame_mbs_only_flag)
-			//mb_adaptive_frame_field_flag =
-			u(1, buf, StartBit);
-
-		//direct_8x8_inference_flag =
-		u(1, buf, StartBit);
-		int frame_cropping_flag = u(1, buf, StartBit);
-		if (frame_cropping_flag) {
-			frame_crop_left_offset = Ue(buf, nLen, StartBit);
-			frame_crop_right_offset = Ue(buf, nLen, StartBit);
-			frame_crop_top_offset = Ue(buf, nLen, StartBit);
-			frame_crop_bottom_offset = Ue(buf, nLen, StartBit);
-		}
-
-		width = (pic_width_in_mbs_minus1 + 1) * 16;
-		height = (2 - frame_mbs_only_flag) * (pic_height_in_map_units_minus1 + 1) * 16;
-
-		if (frame_cropping_flag) {
-			unsigned int crop_unit_x;
-			unsigned int crop_unit_y;
-			if (0 == chroma_format_idc) {
-				// monochrome
-				crop_unit_x = 1;
-				crop_unit_y = 2 - frame_mbs_only_flag;
-			} else if (1 == chroma_format_idc) {
-				// 4:2:0
-				crop_unit_x = 2;
-				crop_unit_y = 2 * (2 - frame_mbs_only_flag);
-			} else if (2 == chroma_format_idc) {
-				// 4:2:2
-				crop_unit_x = 2;
-				crop_unit_y = 2 - frame_mbs_only_flag;
-			} else {
-				// 3 == chroma_format_idc   // 4:4:4
-				crop_unit_x = 1;
-				crop_unit_y = 2 - frame_mbs_only_flag;
-			}
-
-			width -= crop_unit_x * (frame_crop_left_offset + frame_crop_right_offset);
-			height -= crop_unit_y * (frame_crop_top_offset + frame_crop_bottom_offset);
-		}
-
-		int vui_parameter_present_flag = u(1, buf, StartBit);
-		if (vui_parameter_present_flag) {
-			int aspect_ratio_info_present_flag = u(1, buf, StartBit);
-			if (aspect_ratio_info_present_flag) {
-				int aspect_ratio_idc = u(8, buf, StartBit);
-				if (aspect_ratio_idc == 255) {
-					//sar_width =
-					u(16, buf, StartBit);
-					//sar_height =
-					u(16, buf, StartBit);
-				}
-			}
-
-			int overscan_info_present_flag = u(1, buf, StartBit);
-			if (overscan_info_present_flag)
-				//overscan_appropriate_flagu =
-				u(1, buf, StartBit);
-
-			int video_signal_type_present_flag = u(1, buf, StartBit);
-			if (video_signal_type_present_flag) {
-				//video_format =
-				u(3, buf, StartBit);
-				//video_full_range_flag =
-				u(1, buf, StartBit);
-				int colour_description_present_flag = u(1, buf, StartBit);
-				if (colour_description_present_flag) {
-					//colour_primaries =
-					u(8, buf, StartBit);
-					//transfer_characteristics =
-					u(8, buf, StartBit);
-					//matrix_coefficients =
-					u(8, buf, StartBit);
-				}
-			}
-
-			int chroma_loc_info_present_flag = u(1, buf, StartBit);
-			if (chroma_loc_info_present_flag) {
-				//chroma_sample_loc_type_top_field =
-				Ue(buf, nLen, StartBit);
-				//chroma_sample_loc_type_bottom_field =
-				Ue(buf, nLen, StartBit);
-			}
-
-			int timing_info_present_flag = u(1, buf, StartBit);
-			if (timing_info_present_flag) {
-				int num_units_in_tick = u(32, buf, StartBit);
-				int time_scale = u(32, buf, StartBit);
-				fps = time_scale / (2 * num_units_in_tick);
-			}
-		}
-		return true;
-	}
-	return false;
-}
-
 void continueAfterDESCRIBE(RTSPClient* rtspClient, int resultCode, char* resultString);
 void continueAfterSETUP(RTSPClient* rtspClient, int resultCode, char* resultString);
 void continueAfterPLAY(RTSPClient* rtspClient, int resultCode, char* resultString);
@@ -641,7 +461,8 @@ Boolean ourRTMPClient::sendAACFramePacket(u_int8_t* data, unsigned size, double 
 
 //Implementation of "StreamClientState":
 StreamClientState::StreamClientState()
-	:session(NULL), iter(NULL), subsession(NULL), streamTimerTask(NULL), checkAliveTimerTask(NULL), duration(0.0) {
+	:session(NULL), iter(NULL), subsession(NULL), streamTimerTask(NULL), checkAliveTimerTask(NULL),
+	 duration(0.0) {
 }
 
 StreamClientState::~StreamClientState() {
@@ -660,14 +481,24 @@ DummyRTPSink* DummyRTPSink::createNew(UsageEnvironment& env, MediaSubsession& su
 }
 
 DummyRTPSink::DummyRTPSink(UsageEnvironment& env, MediaSubsession& subsession, char const* streamId)
-	: MediaSink(env), fSps(NULL), fPps(NULL), fSpsSize(0), fPpsSize(0), fReceiveBuffer(NULL), fBufferSize(0),
-	  fSubsession(subsession), fHaveWrittenFirstFrame(True) {
+	: MediaSink(env), fSps(NULL), fPps(NULL), fSpsSize(0), fPpsSize(0), fSubsession(subsession),
+	  fHaveWrittenFirstFrame(True), fWidth(640), fHeight(480) {
 	fStreamId = strDup(streamId);
+	fBufferSize = fWidth * fHeight * 1.5 / 8;
+	fReceiveBuffer = new u_int8_t[fBufferSize];
 }
 
 DummyRTPSink::~DummyRTPSink() {
 	delete[] fReceiveBuffer; fReceiveBuffer = NULL;
 	delete[] fStreamId; fStreamId = NULL;
+}
+
+void DummyRTPSink::resetBufferSize(unsigned width, unsigned height){
+	if(width > fWidth || height > fHeight) {
+		fWidth = width;
+		fHeight = height;
+		setBufferSize(width * height * 1.5 / 8);
+	}
 }
 
 void DummyRTPSink::afterGettingFrame(void* clientData, unsigned frameSize,
@@ -684,11 +515,18 @@ void DummyRTPSink::afterGettingFrame(unsigned frameSize, unsigned numTruncatedBy
 	gettimeofday(&scs.lastGettingFrameTime, NULL);
 	double timestamp = fSubsession.getNormalPlayTime(presentationTime) * 1000;
 
+	unsigned width, height, fps;
 	u_int8_t nal_unit_type = fReceiveBuffer[4] & 0x1F; //0xFF;
 
 	if ((strcasecmp(fSubsession.mediumName(), "video") == 0)
 			&& fHaveWrittenFirstFrame) {
 		if (isSPS(nal_unit_type)) {
+			h264_decode_sps(fReceiveBuffer + 4, frameSize, width, height, fps);
+#ifdef DEBUG
+			envir() << "Frame SPS: width:" << width << " height:" << height << " fps:" << fps << "\n";
+#endif
+			resetBufferSize(width, height);
+
 			if (!sendSpsPacket(fReceiveBuffer + 4, frameSize, 0))
 				goto RECONNECT;
 		} else if (isPPS(nal_unit_type)) {
@@ -860,12 +698,9 @@ void continueAfterSETUP(RTSPClient* rtspClient, int resultCode, char* resultStri
 						unsigned width, height, fps;
 						h264_decode_sps(r[n].sPropBytes, r[n].sPropLength, width, height, fps);
 #ifdef DEBUG
-						env << "width: " << width << " height: " << height << " fps: " << fps << "\n";
+						env << "SDP width:" << width << " height:" << height << " fps:" << fps << "\n";
 #endif
-						if (width >= 1280 && height >= 720 )
-							sink->setBufferSize(width * height * 1.5 / 8);
-						else
-							sink->setBufferSize(640 * 480 * 1.5 / 8);
+						sink->resetBufferSize(width, height);
 
 						sink->sendSpsPacket(r[n].sPropBytes, r[n].sPropLength);
 					} else if (isPPS(nal_unit_type)) {
@@ -877,7 +712,7 @@ void continueAfterSETUP(RTSPClient* rtspClient, int resultCode, char* resultStri
 			sink->startPlaying(*(scs.subsession->readSource()), subsessionAfterPlaying, scs.subsession);
 		} else if(strcasecmp(scs.subsession->mediumName(), "audio") == 0) {
 #ifdef DEBUG
-				env << "numChannels:" << scs.subsession->numChannels() << " mode:" << scs.subsession->attrVal_str("mode") << "\n";
+			env << "numChannels:" << scs.subsession->numChannels() << " mode:" << scs.subsession->attrVal_str("mode") << "\n";
 #endif
 			DummyRTPSink* sink = (DummyRTPSink*) scs.subsession->sink;
 			if (strcasecmp(scs.subsession->codecName(), "MPEG4-GENERIC") == 0) {
